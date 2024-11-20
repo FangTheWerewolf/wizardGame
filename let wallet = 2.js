1f38f84573d2a6d428ae6c4cb863eba0208f1850
FangTheWerewolf <cfls3264@gmail.com>
2024-11-19 21:46:50 -0500

    Update

---
+++ let wallet = 2.js
@@ -0,0 +1,12 @@
+let wallet = 2.60
+let price = 5.00
+
+alert("Welcome to the Wizard Game. You better have enough money.")
+
+// You are a dude in a corridor. Your task is to walk down the hallway to find a wizard.
+
+//You ask the wizard- "How much for the wallet? It is leather and I must have it."
+
+//The wizard replies- "It is five dollars."
+
+const 
