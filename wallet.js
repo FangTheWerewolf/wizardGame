84888d8ac6eab5dc521569471e86d7386e84e131
FangTheWerewolf <cfls3264@gmail.com>
2024-11-29 12:43:31 -0500

    Page Delete
    Inactive page deletion in favor of a cleaner one

--- wallet.js
+++ wallet.js
@@ -8,5 +8,3 @@ alert("Welcome to the Wizard Game. You better have enough money.")
 //You ask the wizard- "How much for the wallet? It is leather and I must have it."
 
 //The wizard replies- "It is five dollars."
-
-const 
